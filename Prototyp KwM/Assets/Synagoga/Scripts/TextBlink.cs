c34301ccab54989ef122d7ae7dc58c5692a71e24
Adii020 <adrianswidere8@gmail.com>
2020-07-10 11:30:56 +0200

    Added vr scene
    Added plans (sprite)  used in  system failure simulation
    Added models display, vr set and desk used in Vr scene
    Added blink text script and camera shake used in  system failure simulation

---
+++ Prototyp KwM/Assets/Synagoga/Scripts/TextBlink.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBlink : MonoBehaviour
+{
+    [SerializeField]
+    private Color colorBefore;
+    [SerializeField]
+    private Color colorAfter;
+    [SerializeField]
+    private float blinkingSpeed;
+    private Image image;
+
+    bool changeToColorBefore = false;
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        Blink(); 
+    }
+
+    void Blink()
+    {
+        if (image.color == colorBefore)
+        {
+            changeToColorBefore = false;
+        }
+        else if (image.color == colorAfter)
+        {
+            changeToColorBefore = true;
+        }
+        
+        if (changeToColorBefore)
+        {
+            image.color = Color.Lerp(image.color, colorBefore, Time.deltaTime * blinkingSpeed);
+        }
+        else
+        {
+            image.color = Color.Lerp(image.color, colorAfter, Time.deltaTime * blinkingSpeed);
+        }
+    }
+}
